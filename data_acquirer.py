7bd88e2de36fca45e68050ab2089f773af83c14e
Leo He <43368849+LeoAokma@users.noreply.github.com>
2021-01-04 18:54:18 +0800

    Update data_acquirer.py

--- data_acquirer.py
+++ data_acquirer.py
@@ -10,8 +10,33 @@ python==3.8
 """
 
 from bs4 import BeautifulSoup
+import pandas as pd
 import requests
 import time
+from io import StringIO
+
+
+def convert2table(html_table):
+    """
+    Convert html table to dictionary
+    :param html_table:
+    :return:
+    """
+    table = pd.read_html(html_table)
+    return table
+
+
+def climb(url):
+    isData = True
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36" \
+                 " (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
+    headers = {'User-Agent': user_agent}
+    response = requests.get(url, headers=headers, verify=False)
+    if '404 Error' in response.text:
+        isData = False
+    soup = BeautifulSoup(response.text, 'html.parser')
+    tables = soup.find_all("table")
+    return tables, isData
 
 
 def get_data():
@@ -21,17 +46,20 @@ def get_data():
     """
     i = 1
     while True:
-        try:
-            url = 'https://darkreactions.haverford.edu/database.html?reactions_only=1&page={}'.format(i + 1)
-            user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36" \
-                         " (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
-            headers = {'User-Agent': user_agent}
-            response = requests.get(url, headers=headers, verify=False)
-            # print(response.text)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            content = soup.find_all("tr", class_="quantities")
-            print(content)
-            time.sleep(3)
-            i += 1
-        except Exception:
+        url = 'https://darkreactions.haverford.edu/database.html?reactions_only=1&page={}'.format(i)
+        content, stat = climb(url)
+        if not stat:
             break
+        for _ in content:
+            convert2table(StringIO(str(_)))
+        time.sleep(3)
+        i += 1
+
+"""
+For testing
+"""
+
+content, stat = climb('https://darkreactions.haverford.edu/database.html?reactions_only=1&page=1')
+tables = 
+for _ in content:
+    convert2table(StringIO(str(_)))
