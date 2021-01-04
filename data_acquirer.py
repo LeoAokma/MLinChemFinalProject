b351751243b86f6a87fc16da7a889c6d6063d873
Leo He <43368849+LeoAokma@users.noreply.github.com>
2021-01-04 17:57:52 +0800

    Update data_acquirer.py

--- data_acquirer.py
+++ data_acquirer.py
@@ -9,4 +9,29 @@ Python Environment
 python==3.8
 """
 
-import 
+from bs4 import BeautifulSoup
+import requests
+import time
+
+
+def get_data():
+    """
+    Get database from the website
+    :return: Return acquired data in specific form
+    """
+    i = 1
+    while True:
+        try:
+            url = 'https://darkreactions.haverford.edu/database.html?reactions_only=1&page={}'.format(i + 1)
+            user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36" \
+                         " (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
+            headers = {'User-Agent': user_agent}
+            response = requests.get(url, headers=headers, verify=False)
+            # print(response.text)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            content = soup.find_all("tr", class_="quantities")
+            print(content)
+            time.sleep(3)
+            i += 1
+        except Exception:
+            break
