89bfd1d1bee317f09d322bfcdbef793608ce4a74
Leo He <43368849+LeoAokma@users.noreply.github.com>
2021-01-07 15:54:07 +0800

    Adding functions to todo-list.

--- visualization.py
+++ visualization.py
@@ -3,12 +3,13 @@ author: Leo Aokma
 
 System Environment
 OS: Microsoft Windows 10 Professional x64, WSL with Ubuntu 16.0.4 LTS or MacOS Big Sur 11.0 above
-(No requirments of necessity)
+(No requirements of necessity)
 
 Python Environment
 python==3.8
 """
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def print_matrix(mtx, title=None):
@@ -24,3 +25,30 @@ def print_matrix(mtx, title=None):
         for one in line:
             print("{}\t".format(one), end='')
         print('\n', end='')
+
+
+def testing_status_plot():
+    """
+    Generate the plot of accuracy and recall rate by the progress of training.
+    :return:
+    """
+    # TODO
+    pass
+
+
+def cm_heat_plot():
+    """
+    Generate the heat-point plot of confusion matrix.
+    :return:
+    """
+    # TODO
+    pass
+
+
+def hyper_learning_plot():
+    """
+    Generate the accuracy plot by the progress of regularization coefficient
+    :return:
+    """
+    # TODO
+    pass
