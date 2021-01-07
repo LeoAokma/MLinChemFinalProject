73e6dec97ac4c5d6bdf1f56f46afacbe718e83a0
Leo He <43368849+LeoAokma@users.noreply.github.com>
2021-01-07 15:53:46 +0800

    Fixed some spelling error

--- data_acquirer.py
+++ data_acquirer.py
@@ -3,7 +3,7 @@ author: Leo Aokma
 
 System Environment
 OS: Microsoft Windows 10 Professional x64, WSL with Ubuntu 16.0.4 LTS or MacOS Big Sur 11.0 above
-(No requirments of necessity)
+(No requirements of necessity)
 
 Python Environment
 python==3.8
