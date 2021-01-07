1ded677f91b6a8708aa0df5ecedf4ff390c3d02a
Leo He <43368849+LeoAokma@users.noreply.github.com>
2021-01-07 20:15:42 +0800

    Adding translation

--- data_keys.py
+++ data_keys.py
@@ -25,20 +25,20 @@ test_key = [['time', 'slowCool', 'pH', 'leak', 'numberInorg', 'numberOrg',
             ]
 feat_first = [['orgvanderwaalsMin', 'orgASA+Min', 'orghbdamsdonGeomAvg',
               'PaulingElectronegMean', 'hardnessMaxWeighted', 'AtomicRadiusMeanWeighted'],
-              ['orgvanderwaalsMin', 'orgASA+Min', 'orghbdamsdonGeomAvg',
+              ['orgMin', 'orgASA+Min', 'orghbdamsdonGeom平均值',
                '平均Pauling电负性', '硬度MaxWeighted', '原子半径MeanWeighted']
               ]
 
 feat_top6 = [['time', 'hardnessMinWeighted', 'orgASA_HGeomAvg', 'leak',
              'inorg-water-moleratio', 'orgvanderwaalsArithAvg'],
-             ['反应时间', '硬度MinWeighted', 'orgASA_HGeomAvg', 'leak',
-             '无机组分/水摩尔比', 'org范德华ArithAvg']
+             ['反应时间', '硬度MinWeighted', 'orgASA_H几何平均', 'leak',
+             '无机组分/水摩尔比', 'org范德华算术平均']
              ]
 
 feat_top9 = [['time', 'hardnessMinWeighted', 'orgASA_HGeomAvg', 'leak',
              'inorg-water-moleratio', 'orgvanderwaalsArithAvg',
              'orghbdamsaccMax', 'temp', 'EAMinWeighted'],
-             ['反应时间', '硬度MinWeighted', 'orgASA_HGeomAvg', 'leak',
-             '无机组分/水摩尔比', 'org范德华ArithAvg',
-             'orghbdamsaccMax', '反应温度', 'EAMinWeighted']
+             ['反应时间', '硬度MinWeighted', 'orgASA_H几何平均', 'leak',
+             '无机组分/水摩尔比', 'org范德华算术平均值',
+             'orghbdamsacc最大值', '反应温度', 'EAMinWeighted']
              ]
